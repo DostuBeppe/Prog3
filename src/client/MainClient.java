940680f89663ba0e07bbb6e60ac81359311c942c
Giuseppe Dostuni <giuseppe.dostuni@edu.unito.com>
2019-05-03 14:49:36 +0200

    nuovo messaggio e card

---
+++ src/client/MainClient.java
@@ -0,0 +1,36 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import sample.Main;
+
+import java.io.IOException;
+
+public class MainClient extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    public void initRootLayout() {
+        try {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
